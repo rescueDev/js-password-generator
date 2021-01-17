86da9788978857289beddcd91ba5da9c45fe9e7a
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-17 13:57:25 +0100

    randomize letters numbers and specials

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("password");
+
+//creo variabili con maiusc, min, numbers, specials
+const maiusc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const min = "abcdefghijklmnopqrstuvwxyz";
+const numb = "0123456789";
+const specials = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+/* function geratePass() {
+
+} */
+
+let rndMaiusc = maiusc[Math.round(Math.random() * maiusc.length)];
+let rndMin = min[Math.round(Math.random() * min.length)];
+let rndNumb = numb[Math.round(Math.random() * numb.length)];
+let rndSpecials = specials[Math.round(Math.random() * specials.length)];
+
+console.log(rndMaiusc);
+console.log(rndMin);
+console.log(rndNumb);
+console.log(rndSpecials);
